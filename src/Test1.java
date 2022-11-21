c289458cc0a07e63cb28af0453bdd03acd3abbf1
ah-o-ng12 <ahyoung120613@gmail.com>
2022-11-21 18:02:57 +0900

    push testttttttt

--- src/Test1.java
+++ src/Test1.java
@@ -3,8 +3,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("테스트입니다");
+		System.out.print("�뀒�뒪�듃�엯�땲�떎");
 
+		System.out.println("ahyoung");
 	}
 
 }
