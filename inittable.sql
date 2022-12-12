d9585ca4a6efd743b1cfdddb2cff476981da5ef7
thxto-me <hjd0907@gmail.com>
2022-12-12 16:31:18 +0900

    add inittable.sql

---
+++ inittable.sql
@@ -0,0 +1,30 @@
+drop database if exists network;
+create database network;
+use network;
+
+CREATE TABLE client_list (
+    client_id VARCHAR(10),
+    client_password VARCHAR(100),
+    client_name VARCHAR(11),
+    client_email VARCHAR(20),
+    client_phone VARCHAR(13),
+    client_nick VARCHAR(20),
+    client_birth DATE,
+    salt VARCHAR(40),
+    PRIMARY KEY (client_id)
+);
+
+
+CREATE TABLE login_check (
+    client_id VARCHAR(10),
+    log VARCHAR(7),
+    FOREIGN KEY (client_id)
+        REFERENCES client_list (client_id)
+);
+
+CREATE TABLE client_friend_list (
+    client_id VARCHAR(10),
+    friend_id VARCHAR(10),
+    FOREIGN KEY (client_id)
+        REFERENCES client_list (client_id)
+);
