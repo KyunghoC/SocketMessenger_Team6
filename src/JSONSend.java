4ff6ffec5b249e69416b0071d583aeda74a6b39b
thxto-me <hjd0907@gmail.com>
2022-12-12 14:43:55 +0900

    Json Update

--- src/JSONSend.java
+++ src/JSONSend.java
@@ -1,14 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JSONSend {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
 		Socket socket = null;
 
 		// 연결 요청 //
@@ -20,14 +25,13 @@ public class JSONSend {
 		InputStream is = socket.getInputStream();
 		OutputStream os = socket.getOutputStream();
 
-		// 클라이언트 -> 서버 //
+		// 서버 -> 클라이언트 //
 		JSONObject jsonRoot = new JSONObject(); // jsonObject 생성
-		jsonRoot.put("action", "join"); // 속성명 속성값
+		jsonRoot.put("StatusCode", 01);// 01: 정상, 02: 유저 없음, 03: 비번 틀림 // If문 처리하여 Statcode 따라서 이후 Put, Get 다르게.
 
 		JSONObject jsonData = new JSONObject(); // data 객체 생성
-		jsonData.put("StatusCode", 01);// 01: 정상, 02: 유저 없음, 03: 비번 틀림 // If문 처리하여 Statcode 따라서 이후 Put, Get 다르게.
 		jsonData.put("id", "testID");
-		jsonData.put("name", "trstUserName");
+		jsonData.put("name", "testUserName");
 		jsonData.put("password", "testPassword");
 		jsonData.put("statMessage", "메롱:P");
 		jsonData.put("lastCon", "2022.12.05 12:03:34");
@@ -35,7 +39,7 @@ public class JSONSend {
 
 		jsonRoot.put("data", jsonData); // json 생성
 
-		String json = jsonRoot.toString(); // String 변환
+		String json = jsonRoot.toJSONString(); // String 변환
 
 		System.out.println(json); // 출력해보기
 
@@ -44,6 +48,30 @@ public class JSONSend {
 		pw.println(json); // println을 이용하여 \r\n을 같이 보냄
 		pw.flush(); // flush
 		os.flush();
+
+		// 클라이언트 -> 서버 //
+		is = socket.getInputStream(); // inputStream
+
+		Reader reader = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(reader);
+		String strJson = br.readLine();
+
+		JSONParser parser = new JSONParser();
+
+		JSONObject jsonResult = (JSONObject) parser.parse(strJson); // json객체 선언 (json 파싱)
+		Integer StatusCode = (Integer) jsonResult.get("StatusCode");
+		if (StatusCode == 01) {
+			JSONObject jsoninner = (JSONObject) jsonResult.get("data");
+			String name = (String) jsoninner.get("name");
+			String statMessage = (String) jsoninner.get("statMessage");
+			String time = (String) jsoninner.get("lastCon");
+
+		}
+
+		System.out.println("StatCode: " + Integer.toString(StatusCode)); // action 출력
+
+		is.close(); // InputStream Close
+		os.close();
 
 	}
 
